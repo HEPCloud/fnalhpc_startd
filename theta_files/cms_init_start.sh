8c65a95278fb6c2005f1f93d54f8b3c4e105481e
Maria A <macosta@fnal.gov>
2020-09-04 10:28:31 -0500

    Changing CMD path directly to Launcher

--- theta_files/cms_init_start.sh
+++ theta_files/cms_init_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo clean possible leftovers from previous jobs
+echo "====== Cleaning possible leftovers from previous jobs"
 /usr/bin/fusermount -u /dev/shm/HighLumin/cvmfsexec/dist/cvmfs/config-osg.opensciencegrid.org >& /dev/null
 /usr/bin/fusermount -u /dev/shm/HighLumin/cvmfsexec/dist/cvmfs/unpacked.cern.ch >& /dev/null
 /usr/bin/fusermount -u /dev/shm/HighLumin/cvmfsexec/dist/cvmfs/oasis.opensciencegrid.org >& /dev/null
@@ -9,28 +9,23 @@ rm -rfd /dev/shm/HighLumin >& /dev/null
 
 sleep 5
 
-echo  deploy local squid, start local squid
+echo "====== Deploying and starting local squid"
 mkdir -p /dev/shm/HighLumin
 cd /dev/shm/HighLumin
 tar xzf /projects/HighLumin/frontier-cache_dev_shm_HighLumin.tgz
 /dev/shm/HighLumin/frontier-cache/utils/bin/fn-local-squid.sh start
 /dev/shm/HighLumin/frontier-cache/utils/bin/fn-local-squid.sh status
-echo done
 
-echo start cvmfs and htcondor startd
+echo "====== Configuring CVMFS, if successful, start HTCondor"
 mkdir -p /dev/shm/HighLumin/cvmfs-cache
 cd /dev/shm/HighLumin
 tar xzf /projects/HighLumin/cvmfsexec_dev_shm_HighLumin.tgz
 
-python --version
 
-CMD="source ./node.sh"
 #/dev/shm/HighLumin/cvmfsexec/cvmfsexec cms.cern.ch unpacked.cern.ch oasis.opensciencegrid.org -- ls /cvmfs ; cd ${MY_BASE_DIR} && python ${MY_BASE_DIR}/launcher.py
 /dev/shm/HighLumin/cvmfsexec/cvmfsexec cms.cern.ch unpacked.cern.ch oasis.opensciencegrid.org -- $SHELL -c "ls /cvmfs ; cd ${MY_BASE_DIR} && python ${MY_BASE_DIR}/launcher.py"
 
 
-
-#echo when done stop local squid
-#/dev/shm/HighLumin/frontier-cache/utils/bin/fn-local-squid.sh stop
-#echo clean up, bye
-#rm -rfd /dev/shm/HighLumin >& /dev/null
+echo "===== When Startd exits, stop local squid and cleanup"
+/dev/shm/HighLumin/frontier-cache/utils/bin/fn-local-squid.sh stop
+rm -rfd /dev/shm/HighLumin >& /dev/null
