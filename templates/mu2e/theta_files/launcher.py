36ae951644e953a707792dfced971d86caea1db3
Maria A <mapsacosta@gmail.com>
2021-04-21 19:24:28 +0000

    Logging enhancement for launcher script

--- templates/mu2e/theta_files/launcher.py
+++ templates/mu2e/theta_files/launcher.py
@@ -11,130 +11,108 @@ import signal
 import tarfile
 import subprocess
 import traceback
-#import classad
-#import htcondor
+import pprint
+
+from datetime import datetime
+import platform
 
 WnBaseDir = os.getcwd()
 FsBaseDir = WnBaseDir + "/rendezvous"
 DoneDir = FsBaseDir + "/in_progress"
-#FsBaseDir = os.getenv('SHARED_DIR', WnBaseDir + "/rendezvous")
 ReleaseDir = "/usr"
 #ExecuteDir = WnBaseDir + "/execute"
 ExecuteDir = os.getenv('EXEQ_DIR', WnBaseDir + "/execute")
 LogDir = os.getenv('LOG_DIR', WnBaseDir + "/log")
-#LogDir = WnBaseDir + "/log"
 
 JobList = {}
 
-def DoSubmit( job_name ):
-    print "Submit %s" % job_name
+def DoSubmit( job_name , node_name ):
     full_input_file = os.path.join( FsBaseDir, "%s.tar.gz" % job_name )
     full_execute_dir = os.path.join( ExecuteDir, job_name )
-    if not os.path.isdir(full_execute_dir):
-        try:
-            os.mkdir(full_execute_dir, 0700)
-            print "Created dir -> " + full_execute_dir
-            in_tar = tarfile.open(full_input_file, 'r')
-            in_tar.extractall(full_execute_dir)
-            print "Extracted " + full_input_file + " at " + full_execute_dir
-          #  print "Removing "+ full_input_file +" to "+ os.path.join( DoneDir, "%s.tar.gz" % job_name )
-            #shutil.move( full_input_file, os.path.join( DoneDir, "%s.tar.gz" % job_name ))
-            my_env = os.environ.copy()
-	    my_env["PATH"] = "/usr/local/bin:/sbin:" + my_env["PATH"]
-            my_env["_condor_X509_USER_PROXY"] = full_execute_dir + "/myproxy.pem"
-            my_env["X509_USER_PROXY_STAGEOUT"] = full_execute_dir + "/myproxy.pem"
-            os.environ["_condor_STARTER_JOB_ENVIRONMENT"] = "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin;X509_USER_PROXY_STAGEOUT="+full_execute_dir+"/myproxy.pem"
 
-            starter_args = [ os.path.join( ReleaseDir, "sbin", "condor_starter" ),
-                             "-gridshell",
-                             "-job-input-ad",
-                             os.path.join( full_execute_dir, ".job.ad" ),
-                             "-job-output-ad",
-                             os.path.join( full_execute_dir, ".job.ad.out" )
-                             ]
-            starter = subprocess.Popen(args = starter_args, cwd = full_execute_dir, env=my_env)
-            print "Started standalone condor starter"
-    
-            JobList[job_name] = starter
-        except:
-            print("Unexpected error:", sys.exc_info()[0])
-            traceback.print_exc(file=sys.stdout)
-            # TODO how to indicate failure to submitter
-            return False
-    else:
-        print("Directory already exists")
-        return False 
+    try:
+        os.mkdir(full_execute_dir, 0700)
+        in_tar = tarfile.open(full_input_file, 'r')
+        in_tar.extractall(full_execute_dir)
+        my_env = os.environ.copy()
+        my_env["PATH"] = "/usr/local/bin:/sbin:" + my_env["PATH"]
+        my_env["_condor_X509_USER_PROXY"] = full_execute_dir + "/myproxy.pem"
+        my_env["X509_USER_PROXY_STAGEOUT"] = full_execute_dir + "/myproxy.pem"
+        os.environ["_condor_STARTER_JOB_ENVIRONMENT"] = "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin;X509_USER_PROXY_STAGEOUT="+full_execute_dir+"/myproxy.pem"
+
+        starter_args = [ os.path.join( ReleaseDir, "sbin", "condor_starter" ),
+                         "-gridshell",
+                         "-job-input-ad",
+                         os.path.join( full_execute_dir, ".job.ad" ),
+                         "-job-output-ad",
+                         os.path.join( full_execute_dir, ".job.ad.out" )
+                         ]
+        starter = subprocess.Popen(args = starter_args, cwd = full_execute_dir, env=my_env)
+        print datetime.now().strftime("%d-%m-%Y_%H-%M-%S")+node_name+" Started standalone condor starter"
+        JobList[job_name] = starter
+    except:
+        print(datetime.now().strftime("%d-%m-%Y_%H-%M-%S")+node_name+" Unexpected error:", sys.exc_info()[0])
+        traceback.print_exc(file=sys.stdout)
+        # TODO how to indicate failure to submitter
+        return False
     return True
 
-def DoSendOutput(job_name):
-    print "SendOutput %s" % job_name
+def DoSendOutput( job_name, node_name ):
+    print datetime.now().strftime("%d-%m-%Y_%H-%M-%S")+node_name+" Sending Output "+job_name
     tmp_output_file = os.path.join( ExecuteDir, "%s.out.tar.gz" % job_name )
     full_execute_dir = os.path.join( ExecuteDir, job_name )
 
     try:
         out_tar = tarfile.open(name=tmp_output_file, mode='w:gz')
-        print("Invoking wrapup in this dir: " + full_execute_dir)
+        print(datetime.now().strftime("%d-%m-%Y_%H-%M-%S")+node_name+" Invoking wrapup in this dir: " + full_execute_dir)
         p = subprocess.Popen(["./wrapup_chirp",full_execute_dir], shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, close_fds=True)
         stdout, stderr = p.communicate()
-        print(stdout)
-        print(stderr)
-        #output = p.stdout.read()
-        #print output
-#        subprocess.call("./wrapup_chirp", shell=True)
-#        wrap_up = subprocess.Popen(["./wrapup_chirp"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
- #       wrap_up.wait()
-#        os.system('pwd'
-#        os.system("cat .job.ad.out")
         # TODO skip files older than job start time?
         for job_file in os.listdir(full_execute_dir):
-            #print(job_file)
             out_tar.add(name=os.path.join(full_execute_dir,job_file),
                         arcname=job_file)
         out_tar.close()
         shutil.copy2(tmp_output_file, FsBaseDir)
     except:
-        print "Sending output failed"
-        print("Unexpected error:", sys.exc_info()[0])
+        print(datetime.now().strftime("%d-%m-%Y_%H-%M-%S")+node_name+" Unexpected error:", sys.exc_info()[0])
         traceback.print_exc(file=sys.stdout)
         # TODO how to indicate failure to submitter
         return False
 
     return True
 
-def DoStatusCheck():
+def DoStatusCheck(node_name):
     if len(JobList) == 0:
-        print "  no jobs, skipping check"
+        print datetime.now().strftime("%d-%m-%Y_%H-%M-%S")+node_name+" No jobs, skipping check"
         return True
     try:
         for job_name, starter in JobList.items():
             if starter.poll() != None:
-                print "job %s terminated" % job_name
+                print(datetime.now().strftime("%d-%m-%Y_%H-%M-%S")+node_name+" Job %s terminated" % job_name)
                 del JobList[job_name]
-                DoSendOutput(job_name)
-                DoCleanUp(job_name)
+                DoSendOutput(job_name, node_name)
+                DoCleanUp(job_name, node_name)
     except:
-        print "StatusCheck failed"
-        print("Unexpected error:", sys.exc_info()[0])
+        print datetime.now().strftime("%d-%m-%Y_%H-%M-%S")+node_name+" StatusCheck failed"
+        print(datetime.now().strftime("%d-%m-%Y_%H-%M-%S")+node_name+" Unexpected error:", sys.exc_info()[0])
         traceback.print_exc(file=sys.stdout)
         return False
-    print "StatusCheck succeeded"
+    print datetime.now().strftime("%d-%m-%Y_%H-%M-%S")+node_name+" StatusCheck succeeded"
     return True
 
-def DoCleanUp( job_name ):
-    print "DoCleanUp %s" % job_name
+def DoCleanUp( job_name, node_name ):
+    print datetime.now().strftime("%d-%m-%Y_%H-%M-%S")+node_name+" Cleaning up %s" % job_name
     try:
         if job_name in JobList:
-            # TODO Add better error handling, including SIGKILL of starter after timeout
             proc = JobList[job_name]
             try:
                 os.kill(JobList[job_name], signal.SIGQUIT)
                 (pid, status) = os.waitpid(JobList[job_name], 0)
-                print "  waitpid(%d) returned %d, %d" % (JobList[job_name], pid, status)
+                print datetime.now().strftime("%d-%m-%Y_%H-%M-%S")+node_name+" Waitpid(%d) returned %d, %d" % (JobList[job_name], pid, status)
                 del JobList[job_name]
             except (OSError, TypeError) as e:
-                print("Probably not running on this node")
-                print e
                 pass
+            # TODO Add better error handling, including SIGKILL of starter after timeout
         job_dir = os.path.join(ExecuteDir, job_name)
         if os.access(job_dir, os.F_OK) == True:
             shutil.rmtree(job_dir)
@@ -142,15 +120,14 @@ def DoCleanUp( job_name ):
         if os.access(output_file, os.F_OK) == True:
             os.remove(output_file)
     except:
-        print "Cleanup failed"
-        print("Unexpected error:", sys.exc_info()[0])
+        print datetime.now().strftime("%d-%m-%Y_%H-%M-%S")+node_name+" Cleanup failed"
+        print(datetime.now().strftime("%d-%m-%Y_%H-%M-%S")+node_name+" Unexpected error:", sys.exc_info()[0])
         traceback.print_exc(file=sys.stdout)
         return False
-    print "Cleanup succeeded"
+    print datetime.now().strftime("%d-%m-%Y_%H-%M-%S")+node_name+" Cleanup succeeded"
     return True;
 
 def main():
-    print "====== HTCondor Split Startd Launcher"
 
     # Set environment variables that the starter needs
     os.environ["CONDOR_CONFIG"] = "/dev/null"
@@ -160,27 +137,30 @@ def main():
     os.environ["_condor_STARTER_JOB_ENVIRONMENT"] = "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
     os.environ["_condor_GRIDSHELL_DEBUG"] = "D_PID D_FULLDEBUG"
 
-    import platform
-    print(platform.node())
-    print(platform.platform())
+    node_name = " - "+platform.node() + " (" + os.environ["COBALT_NODEID"]+") - "
 
-    print("THIS IS -- wnbasedir" + WnBaseDir)
-    print("THIS IS -- fsbasedir" + FsBaseDir)
-    print("THIS IS -- execdir" + ExecuteDir)
+    print datetime.now().strftime("%d-%m-%Y_%H-%M-%S")+node_name+" Starting HTCondor Split Startd Launcher"
+    print(datetime.now().strftime("%d-%m-%Y_%H-%M-%S")+node_name+ " Using -- wnbasedir" + WnBaseDir)
+    print(datetime.now().strftime("%d-%m-%Y_%H-%M-%S")+node_name+ "      -- fsbasedir" + FsBaseDir)
+    print(datetime.now().strftime("%d-%m-%Y_%H-%M-%S")+node_name+ "      -- execdir" + ExecuteDir)
+    print(datetime.now().strftime("%d-%m-%Y_%H-%M-%S")+node_name+ "      -- My node ID = " + os.environ["COBALT_NODEID"])
+    print(datetime.now().strftime("%d-%m-%Y_%H-%M-%S")+node_name+ "      -- My slot ID = " + os.environ["SLOT_PREFIX"])
+
     status_write_time = 0
     status_fname = os.path.join(FsBaseDir, "status")
     status_tmp_fname = status_fname + ".tmp"
- #   print("STATUS TEMP FNAME??"+status_tmp_fname)
 
     job_name_prefix = ""
-    if "COBALT_JOBID" in os.environ:
-        job_name_prefix = "slot%d_" % (int(os.environ["COBALT_JOBID"]) + 1)
-  #      print "Using job name prefix '%s'" % job_name_prefix
+
+    if os.environ["COBALT_NODEID"]:
+        job_name_prefix = "slot%d_" % (int(os.environ["COBALT_NODEID"]))
+        print datetime.now().strftime("%d-%m-%Y_%H-%M-%S")+node_name+" Using job name prefix '%s'" % job_name_prefix
+
     while True:
-        print "*** Starting scan ***"
+        print datetime.now().strftime("%d-%m-%Y_%H-%M-%S")+node_name+" *** Starting scan ***"
         print time.ctime()
         if time.time() >= status_write_time + 60:
-            print "Writing status file at "+status_fname
+            print datetime.now().strftime("%d-%m-%Y_%H-%M-%S")+node_name + " Writing status file at "+status_fname
             try:
                 fd = open(status_tmp_fname, "wb")
                 fd.write("WnTime=%d\n" % time.time())
@@ -191,33 +171,30 @@ def main():
             status_write_time = time.time()
         all_input_jobs = set()
         for input_file in os.listdir(FsBaseDir):
-            print "Processing... " + input_file
-    #        print "Matching input file ->" + input_file + " with regexp to job name prefix" + job_name_prefix + ".tgz"
-            m = re.match("([^.]+)\.tar\.gz$", input_file)
+            print datetime.now().strftime("%d-%m-%Y_%H-%M-%S")+node_name + " Processing... " + input_file
+            m = re.match("(%s[^.]+)\.tar\.gz$" % job_name_prefix, input_file)
             if m == None:
-                print("Skipping")
                 continue
             job_name = m.group(1)
+            print(datetime.now().strftime("%d-%m-%Y_%H-%M-%S")+node_name+" Got a job at "+ node_name+ " -> job name "+job_name)
             input_file = os.path.join(FsBaseDir, "%s.tar.gz" % job_name)
             output_file = os.path.join(FsBaseDir, "%s.out.tar.gz" % job_name)
             if os.access(output_file, os.F_OK) == True:
-                print("We finished this job, ignore it")
-                print(job_name)
-                print(output_file)
+                print(datetime.now().strftime("%d-%m-%Y_%H-%M-%S")+node_name+" We finished this job, ignore it")
                 # We finished this job, ignore it
                 continue
             all_input_jobs.add(job_name)
             if job_name not in JobList:
-                print "  Need to submit"
-                rc = DoSubmit(job_name)
+                print datetime.now().strftime("%d-%m-%Y_%H-%M-%S")+node_name+" Submitting "+job_name
+                rc = DoSubmit(job_name,node_name)
 #                print " Submitted -> "+job_name+" now we wait"
                 if rc == False:
-                    print "Skipping, job is running"
+                    print datetime.now().strftime("%d-%m-%Y_%H-%M-%S")+node_name+" Submit failed"
         for removed_job in JobList.viewkeys() - all_input_jobs:
-            print "  Job %s removed by submitter" % removed_job
-            DoCleanUp(removed_job)
+            print datetime.now().strftime("%d-%m-%Y_%H-%M-%S")+node_name+"  Job %s removed by submitter" % removed_job
+            DoCleanUp(removed_job,node_name)
 
-        DoStatusCheck()
+        DoStatusCheck(node_name)
 
         time.sleep(15)
 
