4d9e20db2d19fd1e79591c870702c1751e4b641a
Maria A <mapsacosta@gmail.com>
2021-04-21 19:25:33 +0000

    Logging enhancement for mu2e launcher script

--- templates/mu2e/theta_files/mu2e_init_start.sh
+++ templates/mu2e/theta_files/mu2e_init_start.sh
@@ -14,24 +14,21 @@ function cleanup {
     rm -rfd /tmp/cvmfsexec >& /dev/null
     rm -rfd /local/scratch/mu2e >& /dev/null
     sleep 5
-    echo Done with cleanup
 }
+
+timestamp="$(date +%d-%m-%Y_%H-%M-%S) - $(hostname) ($COBALT_NODEID) - "
 
 # ------- Done -------#
 
 # ------ This is the actual code ------- "
 BASE=${PWD}
 
-echo "==== Running Mu2E Starter script at "
-hostname
+echo "$timestamp Running Mu2e Starter "
 
-echo "====== Cleaning up possible leftovers from previous jobs"
+echo "$timestamp Cleaning up possible leftovers from previous jobs"
 cleanup
-sleep 5
 
-echo "====== Setting relevant environment variables"
-
-echo "====== Configuring CVMFS, if successful, start HTCondor"
+echo "$timestamp Configuring CVMFS, if successful, start HTCondor"
 mkdir -p /local/scratch/mu2e/${SLOT_PREFIX}/cvmfs-cache
 cd /local/scratch/mu2e/${SLOT_PREFIX}
 tar xzf /projects/Mu2e_HEPCloud/cvmfsexec_local_scratch.tgz
@@ -44,14 +41,14 @@ tar xzf /projects/Mu2e_HEPCloud/cvmfsexec_local_scratch.tgz
     cp /lus/theta-fs0/projects/Mu2e_HEPCloud/shared_containers/mu2e_worker_chirp.sif /local/scratch/mu2e/${SLOT_PREFIX}/mu2e_worker_chirp.sif
     EXEC_DIR=${SSD_SCRATCH}/execute
     LOG_DIR=${SSD_SCRATCH}/log
-    echo "Launching CVMFSexec and split starter launcher inside Singularity"
+    echo "$timestamp Launching CVMFSexec and split starter launcher inside Singularity"
     /local/scratch/mu2e/${SLOT_PREFIX}/cvmfsexec/cvmfsexec config-osg.opensciencegrid.org mu2e.opensciencegrid.org fermilab.opensciencegrid.org oasis.opensciencegrid.org -- $SHELL -c "SINGULARITYENV_PATH=/usr/bin:/usr/local/bin:/sbin /cvmfs/oasis.opensciencegrid.org/mis/singularity/bin/singularity exec --env CONDOR_CHIRP=/usr/local/bin/condor_chirp --env LOG_DIR=${LOG_DIR} --bind /etc/hosts --bind /projects/Mu2e_HEPCloud --bind /cvmfs --bind ${LOG_DIR} --home ${BASE} /local/scratch/mu2e/${SLOT_PREFIX}/mu2e_worker_chirp.sif ./launcher.py"
 } || 
 {
-    echo "===== Startd or cvmfsexec exited with errors, cleaning up"
+    echo "$timestamp Startd or cvmfsexec exited with errors, stopping local squid and cleaning up"
     cleanup
     exit 1
 }
 
-echo "===== When Startd exits, do some cleanup"
+echo "$timestamp Startd exiting, stopping local squid and cleaning up"
 cleanup
