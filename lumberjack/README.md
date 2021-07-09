41d774f7c7b4c2553328fb3c4bc66ed8f7b39d1c
Maria A <macosta@thetalogin5.tmi.alcf.anl.gov>
2021-07-09 02:52:57 +0000

    Pushing Lumberjack code upstream

---
+++ lumberjack/README.md
@@ -0,0 +1,18 @@
+## Lumberjack Schedd prototype -- HPC side
+
+# Introduction
+Lumberjack is conceptually similar to HTCondor-C. A set of jobs in the local schedd are flagged as managed by an external scheduler and they end up in some remote schedd for scheduling and execution. Later (usually after the jobs complete), the updated job ad and output files are returned to the local schedd and the external scheduler flag is removed.
+Lumberjack differs from HTCondor-C in several ways:
+* The local jobs are regular vanilla jobs.
+* Explicit “export” and “import" client commands prepare the jobs for movement to another schedd and return the results.
+* The user is responsible for moving all of the job-related files to/from the remote schedd location.
+* No intermediate updates of job status are made to the local schedd.
+* The remote schedd is a newly-created schedd intended to just run these jobs.
+
+# Usage
+
+* Start a singularity instance running a self-contained HTCondor Schedd
+* Make sure the container bind mounts the configured SPOOL directory
+```
+singularity instance start --containall --bind /etc/hosts --bind /lus/grand/projects/HighLumin --env CONDOR_CONFIG=${PWD}/condor_config --home ${PWD} /home/macosta/fnalhpc_startd/containers/htcondor_edge_9_0_0.sif htcondor_${SLOT_PREFIX}
+```
---
+++ lumberjack/local/condor_lumberjack.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python
+import logging
+import htcondor
+import pprint
+import os
+import re
+import sys
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
+os.environ["CONDOR_CONFIG"] = "/etc/condor/condor_config"
+
+
+def acquire_schedd():
+    """Acquire a htcondor.Schedd object
+    Uses the bundled condor_config to connect to the LPC pool, query available schedds,
+    and use the custom `condor_submit` schedd-choosing algorithm to select a schedd for
+    this session. This function will not return the same value, so keep it around until
+    all jobs are removed!
+    """
+    remotePool = re.findall(
+        r"[\w\/\:\/\-\/\.]+", htcondor.param.get("COLLECTOR_HOST")
+    )
+    collector = None
+    scheddAds = None
+    for node in remotePool:
+        try:
+            collector = htcondor.Collector('cmssrv218')
+            scheddAds = collector.query(
+               htcondor.AdTypes.Schedd,
+               projection=[
+                    "Name",
+                    "MyAddress",
+                    "MaxJobsRunning",
+                    "ShadowsRunning",
+                    "RecentDaemonCoreDutyCycle",
+                    "TotalIdleJobs",
+                ],
+                constraint='FERMIHTC_LUMBERJACK_SCHEDD=?=true',
+            )
+            if scheddAds:
+                break
+        except Exception:
+            print(traceback.format_exc())
+            print(sys.exc_info()[2])
+            logger.debug("Failed to contact pool node {node}, trying others...")
+            
+            pass
+
+    if not scheddAds:
+        print("No Schedds available")
+    
+    weightedSchedds = {}
+    for schedd in scheddAds:
+        # covert duty cycle in percentage
+        scheddDC = schedd["RecentDaemonCoreDutyCycle"] * 100
+        # calculate schedd occupancy in terms of running jobs
+        scheddRunningJobs = (schedd["ShadowsRunning"] / schedd["MaxJobsRunning"]) * 100
+
+        logger.debug("Looking at schedd: " + schedd["Name"])
+        logger.debug(f"DutyCyle: {scheddDC}%")
+        logger.debug(f"Running percentage: {scheddRunningJobs}%")
+        logger.debug(f"Idle jobs: {schedd['TotalIdleJobs']}")
+
+        # Calculating weight
+        # 70% of schedd duty cycle
+        # 20% of schedd capacity to run more jobs
+        # 10% of idle jobs on the schedd (for better distribution of jobs across all schedds)
+        weightedSchedds[schedd["Name"]] = (
+            (0.7 * scheddDC)
+            + (0.2 * scheddRunningJobs)
+            + (0.1 * schedd["TotalIdleJobs"])
+        )
+
+    schedd = min(weightedSchedds.items(), key=lambda x: x[1])[0]
+    schedd = collector.locate(htcondor.DaemonTypes.Schedd, schedd)
+    return htcondor.Schedd(schedd)
+
+if __name__=="__main__":
+  parser = argparse.ArgumentParser(description='Executes a polled query to an HTCondor Collector for Schedd ClassAds')
+  parser.add_argument('-jobconstraint', type=str,help='Constraint expression (String) for selecting jobs to export')
+  parser.add_argument('-ids', nargs='+', type=int,help='Space separated list of ClusterIDs (Int) to export')
+  parser.add_argument('-out', type=str,help='Output directory for the exported job file')
+  parser.add_argument('-remotespool', type=str,help='Path of the SPOOL directory on the remote Schedd')
+
+  const = args.jobconstraint
+  # To show the results of the given option to screen.
+  for _, value in parser.parse_args()._get_kwargs():
+      if value is not None:
+          print(value)
+
+  SCHEDD = acquire_schedd()
+#  SCHEDD.export_jobs(job_spec=<constraint string or list of cluster ids>, export_dir=“/path/to/export”, new_spool_dir=“##”)
